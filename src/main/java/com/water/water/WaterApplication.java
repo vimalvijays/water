def79bb718bd0df1f20e171ac5f3ed3dbe6164cc
vimalvijays <vimal.m@hashworks.co>
2017-08-04 11:19:05 +0530

    testing

--- src/main/java/com/water/water/WaterApplication.java
+++ src/main/java/com/water/water/WaterApplication.java
@@ -3,7 +3,7 @@ package com.water.water;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//spring main class testing
+//spring main class testing second
 @SpringBootApplication
 public class WaterApplication {
 
