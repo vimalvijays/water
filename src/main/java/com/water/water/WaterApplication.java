0de734c752d2939662e5a6c76d74c5a5da0e9dfb
vimalvijays <vimal.m@hashworks.co>
2017-08-04 11:32:04 +0530

    testing

--- src/main/java/com/water/water/WaterApplication.java
+++ src/main/java/com/water/water/WaterApplication.java
@@ -3,7 +3,7 @@ package com.water.water;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//spring main class testing second
+//spring main class testing second check slave
 @SpringBootApplication
 public class WaterApplication {
 
